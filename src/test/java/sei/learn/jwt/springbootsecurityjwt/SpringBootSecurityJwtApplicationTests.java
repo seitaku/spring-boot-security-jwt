2e38875c025b912693699a08886b92b87a779579
Sei <seitaku@wethink.com.tw>
2021-05-18 17:08:50 +0800

    init

---
+++ src/test/java/sei/learn/jwt/springbootsecurityjwt/SpringBootSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package sei.learn.jwt.springbootsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
